dc7208b8eeb9b7995af9493802951636f34cd3e8
Unknown <chris_albin@outlook.com>
2018-10-10 12:10:37 +0200

    Added healthbar and enemy prefab. Changed sprites on prefabs

---
+++ Plumber Man/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class HealthManager : MonoBehaviour {
+    public int health;
+    public int numbofHearts;
+
+    public Image[] hearts;
+    public Sprite full_Heart;
+    public Sprite empty_Heart; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdatePlayerHealth();
+        DrawHearts();
+    }
+
+    public void UpdatePlayerHealth()
+    {
+            print("Current health is: " + health 
+                + " Current numberOfHearts is: " + numbofHearts); 
+        if (health > numbofHearts)
+        {
+            health = numbofHearts;
+        }
+    }
+
+    public void DrawHearts()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = full_Heart;
+            }
+            else
+            {
+                hearts[i].sprite = empty_Heart;
+            }
+
+            if (i < numbofHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
+    }
+
+}
+
+
