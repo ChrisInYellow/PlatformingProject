cab9d2f164b27fa3338041e3b440f69b4994cd61
Unknown <chris_albin@outlook.com>
2018-10-11 16:33:11 +0200

    WIP LevelBuilder

---
+++ Plumber Man/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject[] objects;
+    private GameObject test; 
+
+	void Start () {
+        int rand = Random.Range(0, objects.Length);
+        Instantiate(objects[rand], transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
