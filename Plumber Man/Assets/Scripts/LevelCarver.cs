fab3ada137e756aaee6c16b640e41eada01aa322
Unknown <chris_albin@outlook.com>
2018-10-16 13:46:07 +0200

    WIP Level Carver

--- Plumber Man/Assets/Scripts/LevelCarver.cs
+++ Plumber Man/Assets/Scripts/LevelCarver.cs
@@ -9,10 +9,15 @@ public class LevelCarver : MonoBehaviour {
     public GameObject[] rooms;
 
     public float moveAmount;
-    public float startTimeBtwRoom = 0.25f; 
+    public float startTimeBtwRoom = 0.25f;
+
+    public float minX;
+    public float maxX;
+    public float minY; 
 
     private int direction;
-    private float timeBtwRoom; 
+    private float timeBtwRoom;
+    private bool levelFinished; 
 
 	// Use this for initialization
 	void Start () {
@@ -25,7 +30,7 @@ public class LevelCarver : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (timeBtwRoom <= 0)
+        if (timeBtwRoom <= 0 && levelFinished == false)
         {
             CarveRoute();
             timeBtwRoom = startTimeBtwRoom; 
@@ -39,18 +44,39 @@ public class LevelCarver : MonoBehaviour {
     {
         if(direction == 1 || direction == 2)
         {
-            Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
-            transform.position = newPos; 
+            if(transform.position.x < maxX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+                transform.position = newPos; 
+            }
+            else
+            {
+                direction = 5; 
+            }
         }
         else if(direction == 3 || direction ==4)
         {
-            Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
-            transform.position = newPos;
+            if(transform.position.x > minX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+                transform.position = newPos;
+            }
+            else
+            {
+                direction = 5; 
+            }
         }
         else if(direction == 5)
         {
-            Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
-            transform.position = newPos;
+            if (transform.position.y>minY)
+            {
+                Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
+                transform.position = newPos;
+            }
+            else
+            {
+                levelFinished = true; 
+            }
         }
 
         Instantiate(rooms[0], transform.position, Quaternion.identity);
