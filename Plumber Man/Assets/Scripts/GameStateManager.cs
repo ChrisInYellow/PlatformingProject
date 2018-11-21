8d41444a1dc413486ef4ece8d9e3741f0827138c
Unknown <chris_albin@outlook.com>
2018-11-21 16:59:18 +0100

    Added Game State Manager

---
+++ Plumber Man/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class GameStateManager : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        SceneTransitions();
+	}
+
+    public void SceneTransitions()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            {
+            Application.Quit();
+        }
+    }
+}
